85192d525ae580514056785dc653a17a50fded93
David king <phoenixdkd2004@gmail.com>
2024-04-13 13:30:06 +0100

    added the footer, footer wrapper and the details. i also added some javascript functionalities as well

--- app.js
+++ app.js
@@ -1,3 +1,20 @@
+// change navbar color
+$(document).ready(function(){
+    $(window).scroll(function(){
+        var scroll = $(window).scrollTop();
+        if(scroll>150) {
+            $(".navbar").css("background", "#222");
+            $(".navbar").css("box-shadow", "rgba(0, 0, 0, 0.1) 0px 4px 12px");
+        }
+        else {
+            $(".navbar").css("background","transparent");
+            $(".navbar").css("box-shadow", "none");
+        }
+    })
+})
+
+
+
 // swiper
 var swiper = new Swiper(".mySwiper",
 {
